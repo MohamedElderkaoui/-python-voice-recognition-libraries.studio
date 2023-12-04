c5307799dcf6ea52711fc58d0d8cd76d6a751235
MohamedElderkaoui <mderkaoui10@gmail.com>
2023-12-04 14:03:44 +0100

    .

--- SACA.py
+++ SACA.py
@@ -3,13 +3,20 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 harvard = sr.AudioFile('./harvard.wav')
+moha = sr.AudioFile('./moha2.wav')
+
 with harvard as source:
     r.adjust_for_ambient_noise(source)
-    audio1 = r.record(source, duration=4)
+    audio1 = r.record(source, duration=40)
+
+with moha as source:
+    r.adjust_for_ambient_noise(source)
+    audio2 = r.record(source, duration=4)
 
 # r.recognize_google(audio1)
 
 print(r.recognize_google(audio1))
+print(r.recognize_google(audio2))
 
 # audio2 = r.record(source, duration=4)
 
